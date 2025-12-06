ebd5a9b20cb97cc5ae3684e5376deaa4da0e0b02
Clifford <140809540+omari91@users.noreply.github.com>
2025-12-06 18:11:48 +0100

    Delete tests/test_fuzzy_controller.py

--- tests/test_fuzzy_controller.py
+++
@@ -1,9 +0,0 @@
-from src.fuzzy_controller import sigmoid_dimming
-
-
-def test_sigmoid_bounds():
-    limit = 50.0
-    for l in [5, 25, 50, 70, 120]:
-        val = sigmoid_dimming(l, limit)
-        assert isinstance(val, float)
-        assert val <= limit * 1.05
